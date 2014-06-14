f4e2062ab5db22039f60b1172936497d61724f6b
Austen Ito <austen.ito@gmail.com>
2014-06-14 12:07:17 -0400

    move command to dockerfile and shell script

--- recipes/happiness-service.rb
+++ recipes/happiness-service.rb
@@ -14,6 +14,7 @@ docker_image 'ubuntu' do
   # source 'https://raw.githubusercontent.com/austenito/happiness-service-docker/master/Dockerfile'
   source '/vagrant/docker-files/happiness-service/Dockerfile'
   action :build
+  cmd_timeout 900
 end
 
 docker_container 'happiness-service' do
@@ -32,35 +33,33 @@ end
 docker_container 'happiness-service' do
   image 'ubuntu:happiness-service'
   container_name "happiness-service"
-  command "bash -c 'source /usr/local/share/chruby/chruby.sh && \
-                    git clone https://github.com/austenito/happiness_service.git && \
-                    cd happiness_service && \
-                    chruby 2.1.2 && \
-                    bundle install && \
-                    rake db:create db:migrate'"
-  detach false
-  working_directory '/home'
-  cmd_timeout 600
+  # command "bash -c 'source /usr/local/share/chruby/chruby.sh && \
+                    # git clone https://github.com/austenito/happiness_service.git && \
+                    # cd happiness_service && \
+                    # chruby 2.1.2 && \
+                    # bundle install && \
+                    # rake db:create db:migrate'"
+  detach true
   env "POSTGRES_USER=#{ENV['POSTGRES_USER']} POSTGRES_PASSWORD=#{ENV['POSTGRES_PASSWORD']}"
 end
 
-timestamp = Time.new.strftime('%Y%m%d%H%M')
+# timestamp = Time.new.strftime('%Y%m%d%H%M')
 
 # Commit container changes
-docker_container 'happiness-service' do
-  repository 'ubuntu'
-  tag "happiness-service-#{timestamp}"
-  action :commit
-end
+# docker_container 'happiness-service' do
+  # repository 'ubuntu'
+  # tag "happiness-service-#{timestamp}"
+  # action :commit
+# end
 
-docker_container "happiness-service" do
-  image "ubuntu:happiness-service-#{timestamp}"
-  container_name "happiness-service-#{timestamp}"
-  command "bash -c 'source /usr/local/share/chruby/chruby.sh && \
-                    chruby 2.1.2 && \
-                    unicorn -p 3000'"
-  port '3000:3000'
-  detach true
-  working_directory '/home/happiness_service'
-  env "POSTGRES_USER=#{ENV['POSTGRES_USER']} POSTGRES_PASSWORD=#{ENV['POSTGRES_PASSWORD']}"
-end
+# docker_container "happiness-service" do
+  # image "ubuntu:happiness-service-#{timestamp}"
+  # container_name "happiness-service-#{timestamp}"
+  # command "bash -c 'source /usr/local/share/chruby/chruby.sh && \
+                    # chruby 2.1.2 && \
+                    # unicorn -p 3000'"
+  # port '3000:3000'
+  # detach true
+  # working_directory '/home/happiness_service'
+  # env "POSTGRES_USER=#{ENV['POSTGRES_USER']} POSTGRES_PASSWORD=#{ENV['POSTGRES_PASSWORD']}"
+# end
