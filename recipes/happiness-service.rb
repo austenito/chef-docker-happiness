f3e939c53a0b406ab94e938f5edc0316db7453c4
Austen Ito <austen.ito@gmail.com>
2014-06-14 13:26:47 -0400

    idempotent container deploys

--- recipes/happiness-service.rb
+++ recipes/happiness-service.rb
@@ -13,26 +13,18 @@ docker_image 'ubuntu' do
   tag 'happiness-service'
   # source 'https://raw.githubusercontent.com/austenito/happiness-service-docker/master/Dockerfile'
   source '/vagrant/docker-files/happiness-service/Dockerfile'
-  action :build
+  action :build_if_missing
   cmd_timeout 900
 end
 
-docker_container 'happiness-service' do
-  action :stop
-  force true
-end
-
-docker_container 'happiness-service' do
-  action :remove
-end
-
-execute 'remove cid' do
-  command 'rm -f /var/run/happiness-service*.cid'
-end
+docker_container('happiness-service') { action :stop }
+docker_container('happiness-service') { action :remove }
+execute('remove cid') { command 'rm -f /var/run/happiness-service.cid' }
 
 docker_container 'happiness-service' do
   image 'ubuntu:happiness-service'
   container_name "happiness-service"
   detach true
   env "POSTGRES_USER=#{ENV['POSTGRES_USER']} POSTGRES_PASSWORD=#{ENV['POSTGRES_PASSWORD']}"
+  action :run
 end
--- recipes/postgres.rb
+++ recipes/postgres.rb
@@ -13,34 +13,26 @@ docker_image 'ubuntu' do
   tag 'postgres-data'
   # source 'https://raw.githubusercontent.com/austenito/happiness-service-docker/master/Dockerfile'
   source '/vagrant/docker-files/postgres-data'
-  action :build
+  action :build_if_missing
 end
 
 docker_container 'postgres-data' do
   image 'ubuntu:postgres-data'
   container_name 'postgres-data'
   detach true
-end
-
-docker_container 'postgres-production' do
-  action :stop
-  force true
-end
-
-docker_container 'postgres-production' do
-  action :remove
-end
-
-execute 'remove cid' do
-  command 'rm -f /var/run/postgres-production*.cid'
+  action :run
 end
 
 docker_image 'ubuntu' do
   tag 'postgres-production'
   # source 'https://raw.githubusercontent.com/austenito/happiness-service-docker/master/Dockerfile'
   source '/vagrant/docker-files/postgres/Dockerfile'
-  action :build
+  action :build_if_missing
 end
+
+docker_container('postgres-production') { action :stop }
+docker_container('postgres-production') { action :remove }
+execute('remove cid') { command 'rm -f /var/run/postgres-production.cid' }
 
 docker_container 'postgres-production' do
   image 'ubuntu:postgres-production'
@@ -49,4 +41,5 @@ docker_container 'postgres-production' do
   detach true
   env ["POSTGRES_USER=#{ENV['POSTGRES_USER']}", "POSTGRES_PASSWORD=#{ENV['POSTGRES_PASSWORD']}"]
   volumes_from 'postgres-data'
+  action :run
 end
