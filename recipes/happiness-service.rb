200aa23bde2f8157da67708aa70098effd658307
Austen Ito <austen.ito@gmail.com>
2014-06-14 12:08:28 -0400

    Remove commented out stuff

--- recipes/happiness-service.rb
+++ recipes/happiness-service.rb
@@ -33,33 +33,6 @@ end
 docker_container 'happiness-service' do
   image 'ubuntu:happiness-service'
   container_name "happiness-service"
-  # command "bash -c 'source /usr/local/share/chruby/chruby.sh && \
-                    # git clone https://github.com/austenito/happiness_service.git && \
-                    # cd happiness_service && \
-                    # chruby 2.1.2 && \
-                    # bundle install && \
-                    # rake db:create db:migrate'"
   detach true
   env "POSTGRES_USER=#{ENV['POSTGRES_USER']} POSTGRES_PASSWORD=#{ENV['POSTGRES_PASSWORD']}"
 end
-
-# timestamp = Time.new.strftime('%Y%m%d%H%M')
-
-# Commit container changes
-# docker_container 'happiness-service' do
-  # repository 'ubuntu'
-  # tag "happiness-service-#{timestamp}"
-  # action :commit
-# end
-
-# docker_container "happiness-service" do
-  # image "ubuntu:happiness-service-#{timestamp}"
-  # container_name "happiness-service-#{timestamp}"
-  # command "bash -c 'source /usr/local/share/chruby/chruby.sh && \
-                    # chruby 2.1.2 && \
-                    # unicorn -p 3000'"
-  # port '3000:3000'
-  # detach true
-  # working_directory '/home/happiness_service'
-  # env "POSTGRES_USER=#{ENV['POSTGRES_USER']} POSTGRES_PASSWORD=#{ENV['POSTGRES_PASSWORD']}"
-# end
