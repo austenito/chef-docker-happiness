47705ced99d27354c3eb678d71a6926290b39413
Austen Ito <austen.ito@gmail.com>
2014-07-15 08:34:12 -0400

    Move default attributes to node file

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,13 +1,8 @@
-default['postgres']['_default']['user'] = 'nyan'
-default['postgres']['_default']['password'] = 'cat'
-default['postgres']['production']['user'] = ""
-default['postgres']['production']['password'] = ""
+default['postgres']['user'] = ""
+default['postgres']['password'] = ""
 
-default['poptart']['_default']['token'] = "e12281b7412a007631588a79d73aa402da5aeeeba93ae76e951c781f733b1a05"
-default['poptart']['production']['token'] = ""
+default['poptart']['token'] = ""
 
-default['logentries']['happiness_service']['_default']['token'] = "2203a941-2270-4562-a3cd-4442e447cc57"
-default['logentries']['happiness_service']['production']['token'] = ""
+default['logentries']['happiness_service']['token'] = ""
 
-default['logentries']['happiness']['_default']['token'] = "2203a941-2270-4562-a3cd-4442e447cc57"
-default['logentries']['happiness']['production']['token'] = ""
+default['logentries']['happiness']['token'] = ""
